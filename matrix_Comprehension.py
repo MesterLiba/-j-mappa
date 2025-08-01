5495405b5021b29e6211f6dddd90b2d0fee8e7ab
Mesterliba <pinerpisti99@gmail.com>
2025-08-01 18:01:18 +0200

    matrixkisupdate

--- matrix_Comprehension.py
+++ matrix_Comprehension.py
@@ -13,57 +13,27 @@ def change_mark():
         return 'o'
 
 def check_board():
-    if '_' not in board_alap:
+    if '_' in board_alap:
+        return True
+    else:
         return False
-    return True  
- 
-playing = True
+    
 step = 'not_valid'
 lepes_szam = 0
 
-while playing == True:
-    while step != 'valid' :
-        p1i = int(input(f"Hova szeretnéd rakni 1-9 : "))-1
-        if board_alap[p1i] == '_':
-            board_alap[p1i] = change_mark()
-            step = 'valid'
-            lepes_szam +=1
-            print_board()
-            playing = check_board()
-        else:
-            print("Ez a mező már foglalt!")
-        step = 'not_valid'
-        
 
-    
-            
-
-
-
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-# def for_loop(n):
-#     my_list = []
-#     for x in range(n):
-#         my_list.append(x)
-#     return my_list
-
-# result = for_loop(25)
-
-# my_new_list = [x for x in range(21) if x %2 == 0 ]
-# print(my_new_list)
+while check_board() == True:
+    while step != 'valid':
+        try:
+            p1i = int(input(f"Hova szeretnéd rakni 1-9 : "))-1
+            if board_alap[p1i] == '_':
+                board_alap[p1i] = change_mark()
+                step = 'valid'
+                lepes_szam +=1
+                print_board()
+            else:
+                print('ez a mezö már foglalt')
+        except ValueError:
+            print("Hiba valamit roszul adtál meg")
+    step = 'not_valid'
+print('A tábla megtelt döntetlen.')
